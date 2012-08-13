e7290b023439a46775b6b19c32af1c1b9065eab3
Tulia Alexey <alexey.tulia@gmail.com>
2012-08-13 15:20:45 +0300

    Files renamed.

--- lib/karaoke/basic.rb
+++ lib/karaoke/basic.rb
@@ -159,33 +159,5 @@ module Karaoke
 
 			p result.to_json
 		end
-
-		post '/apply' do
-			id = params[:id]
-			table = params[:table]
-			track = params[:track]
-			
-			hash_data ||= {}
-
-			hash_data = {
-			 	"id" => id,
-			 	"table" => table,
-			 	"track" => track
-			 }
-
- 			hash_item =  @@array.find{|f| f["id"].eql?(id)}
-			
-			unless hash_item 
-				puts "Added to array"
-				@@array << hash_data
-			else
-				hash_item["table"] = table
-				hash_item["track"] = track
-			end
-
-			puts @@array
-
-			redirect :admin
-		end
 	end
 end
